7cb805d127d4585fc959be434af9ee594d438591
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-06-29 16:36:34 +0530

    javascript changes

--- script.js
+++ script.js
@@ -9,45 +9,99 @@ const contact_name = document.getElementById("contact-name");
 const contact_email = document.getElementById("contact-email");
 const contact_text = document.getElementById("contact-text");
 const contact_button = document.getElementById("contact-send-btn");
+const livebox = document.getElementById("livebox");
 
-contact_button.onclick = function () {
+contact_button.addEventListener("click",validinfo);
+
+function validinfo(){
+  if(
+  validname()&&
+  validemail()&&
+  validmessage()
+  )
+  {
+    alert("Successfully Submitted!!");
+    reset();
+  }
+}
+
+function reset(){
+contact_name.value="";
+contact_name.style.border="";
+contact_email.value="";
+contact_email.style.border="";
+contact_text.value="";
+contact_text.style.border="";
+}
+
+
+function validname() {
   const name = contact_name.value.trim();
+  const validname1=/^[A-Za-z ]{3,25}$/;
+
   if (name == "") {
-    alert("Name must be Required!");
-    return;
+    livebox.style.display="block";
+     livebox.textContent="Name Required";
+     contact_name.style.border="2px solid red";
+    return false;
   }
-   else if (name.length > 25) {
-    alert("Name is too long!!");
-    return;
+   else if (!validname1.test(name)) {
+     livebox.style.display="block";
+      contact_name.style.border="2px solid red";
+     livebox.textContent="Name is to Long!!";
+    return false;
   }
+  
+  else{
+     livebox.style.display="none";
+   contact_name.style.border="2px solid green";
+   return true;
+  }
+}
 
+function validemail(){
   const email = contact_email.value.trim();
-  const validname = /^[A-Za-z ]{3,}$/;
-  if (!validname.test(name)) {
-    alert("InValid Name!!");
-    return;
-  }
-   else if (email == "") {
-    alert("Email must be Required");
-    return;
+   const validemail = /^[A-Za-z0-9-_+%.]+@[A-Za-z0-9-_.]+\.[A-Za-z]{2,}$/;
+
+   if (email == "") {
+     livebox.style.display="block";
+      contact_email.style.border="2px solid red";
+    livebox.textContent="Email Required";
+    return false;
   }
 
-  const validemail = /^[A-Za-z0-9-_+%.]+@[A-Za-z0-9-_.]+\.[A-Za-z]{2,}$/;
-  if (!validemail.test(email)) {
-    alert("Email InValid");
-    return;
+ 
+  else if (!validemail.test(email)) {
+     livebox.style.display="block";
+      contact_email.style.border="2px solid red";
+   livebox.textContent="Email Invalid!!";
+    return false;
   }
+  else{
+  livebox.style.display="none";
+      contact_email.style.border="2px solid green";
+      return true;
+  }
+}
 
+function validmessage(){
   const message = contact_text.value.trim();
   if (message == "") {
-    alert("Enter your message!!");
-    return;
+     livebox.style.display="block";
+      contact_text.style.border="2px solid red";
+    livebox.textContent="Message Required";
+    return false;
   } 
   else if (message.length > 100) {
-    alert("message must have letters less than 100.");
-    return;
+     livebox.style.display="block";
+      contact_text.style.border="2px solid red";
+     livebox.textContent="message must only have letters less than 100.";
+    return false;
   } 
-  
-    alert("Contact Details send Successfully!!");
+  else{
+    livebox.style.display="none";
+      contact_text.style.border="2px solid green";
+      return true;
+  }
   
 };
