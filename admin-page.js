b5e8b9c89d58cbf7d0e4384d5b2bbe3a475e182e
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-07-03 14:26:02 +0530

    new js added

--- admin-page.js
+++ admin-page.js
@@ -1,14 +1,39 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-
-menuBtn.onclick = function () {
-  navLinks.classList.toggle("active");
-};
-
 const tablecontrols = document.getElementById("table-controls");
-const searchbox = document.getElementById("search-box");
+const searchbox = document.getElementById("search-box-main");
 const filterbox = document.getElementById("filter-box");
 const complaintable = document.getElementById("complaint-table");
-const table = document.getElementById("table");
 const tablerow = document.getElementById("table-row");
-const viewbtn = document.getElementById("view-btn");
+const rows = document.querySelectorAll("#table-body tr");
+const viewbtn = document.getElementsByClassName("view-btn");
+
+searchbox.addEventListener("input", search_complaint);
+filterbox.addEventListener("change", filter_complaint);
+
+function filter_complaint() {
+  const value = filterbox.value;
+
+  rows.forEach(function (row) {
+    const status = row.cells[4].textContent;
+
+    if (value === "All") {
+      row.style.display = "";
+    } else if (value === status) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+}
+
+function search_complaint() {
+  const value = searchbox.value.toUpperCase();
+  rows.forEach(function (row) {
+    const complaintid = row.cells[0].textContent.toUpperCase();
+
+    if (complaintid.includes(value)) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+}
