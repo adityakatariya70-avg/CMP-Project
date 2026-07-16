50679296f82ed39473ee3a208ff1fcb4422c3400
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-07-16 20:10:07 +0530

    Completed backend setup with MongoDB, User model and user routes

---
+++ Backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async()=>{
+    try{
+        console.log(process.env.MongoDB);
+        await mongoose.connect(process.env.MongoDB);
+        console.log("Mongo DB Connected Successfully");
+    }
+    catch(error){
+    console.log("Mongo DB Connection Failed!!");
+    console.log(error);
+    process.exit(1);
+    }
+
+}
+module.exports=connectDB;
---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors=require("cors");
+require("dotenv").config();
+const connectDB=require("./config/db");
+const userRoutes=require("./routes/userRoutes");
+
+const app=express();
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use("/api/users",userRoutes);
+
+app.get('/',(req,res)=>{
+   res.send("Complaint hub is running on port 5000");
+})
+
+const Port= process.env.PORT || 8000;
+app.listen(Port,()=>{
+    console.log(`Server is running on Port ${Port}`);
+}); 
