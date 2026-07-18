748a8460fdf0fec7dd800cf6c8bb8b55f9524607
adityakatariya70-avg <adityakatariya70@gmail.com>
2026-07-18 22:02:06 +0530

    All backend api created

--- Backend/controllers/adminComplaintController.js
+++ Backend/controllers/adminComplaintController.js
@@ -1,74 +1,109 @@
-const complaintModel =require("../models/complaintModel");
+const complaintModel = require("../models/complaintModel");
 
-const getallComplaints= async (req,res)=>{
-    try{
-  const allComplaints= await complaintModel.find();
+const getallComplaints = async (req, res) => {
+  try {
+    const allComplaints = await complaintModel.find();
 
-  if(allComplaints.length===0){
-    return res.status(404).json({
-        message:"No Complaints Found"
-    })
-  }
-  return res.status(200).json({
-    message:"All Complaints",
-    Complaints: allComplaints
-  })
+    if (allComplaints.length === 0) {
+      return res.status(404).json({
+        message: "No Complaints Found",
+      });
     }
-    catch(error){
- return res.status(500).json({
-    message:"Server not Working"
- })
-    }
-}
-
-const getsingleComplaint= async (req,res)=>{
-    try{
-
-
-        const {complaintId}=req.params;
-        const complaint = await complaintModel.findOne({complaintId});
-        if(!complaint){
-         return res.status(404).json({
-            message:"Complaint Not Found"
-         })
-
-        }
-        return res.status(200).json({
-            message:"Complaint Details",
-            complaint:complaint
-        })
-    }
-    catch(error){
+    return res.status(200).json({
+      message: "All Complaints",
+      Complaints: allComplaints,
+    });
+  } catch (error) {
     return res.status(500).json({
-        message:"Internal Server Error"
+      message: "Server not Working",
+    });
+  }
+};
+
+const getsingleComplaint = async (req, res) => {
+  try {
+    const { complaintId } = req.params;
+    const complaint = await complaintModel.findOne({ complaintId });
+    if (!complaint) {
+      return res.status(404).json({
+        message: "Complaint Not Found",
+      });
+    }
+    return res.status(200).json({
+      message: "Complaint Details",
+      complaint: complaint,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const updatecomplaintStatus = async (req, res) => {
+  try {
+    const { complaintId } = req.params;
+    const { status } = req.body;
+    const updatedComplaint = await complaintModel.findOneAndUpdate(
+      { complaintId },
+      { status },
+      { new: true },
+    );
+
+    if (!updatedComplaint) {
+      return res.status(404).json({
+        message: "Complaint Not Found",
+      });
+    }
+    return res.status(200).json({
+      message: "Complaint Updated Successfully",
+      updateComplaint: updatedComplaint,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const dashboardStats = async (req, res) => {
+  try {
+    const totalComplaints = await complaintModel.countDocuments();
+
+    const pendingComplaints = await complaintModel.countDocuments({
+      status: "Pending",
+    });
+
+    const inprogressComplaints = await complaintModel.countDocuments({
+      status: "In Progress",
+    });
+
+    const resolvedComplaints = await complaintModel.countDocuments({
+      status: "Resolved",
+    });
+    const rejectedComplaints= await complaintModel.countDocuments({
+        status:"Rejected"
     })
-    }
-}
+    return res.status(200).json({
+      message: "Dashboard Statistics",
+     stats:{
+        totalComplaints,
+        pendingComplaints,
+        inprogressComplaints,
+        resolvedComplaints,
+        rejectedComplaints
+     }
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
 
-
-const updatecomplaintStatus= async (req,res)=>{
-    try{
- const {complaintId}=req.params;
- const {status}=req.body;
- const updatedComplaint= await complaintModel.findOneAndUpdate({complaintId},{status},{new:true});
- 
- if(!updatedComplaint){
-    return res.status(404).json({
-        message:"Complaint Not Found"
-    })
- }
- return res.status(200).json({
-    message:"Complaint Updated Successfully",
-    updateComplaint:updatedComplaint
- })
-    }
-    catch(error){
-return res.status(500).json({
-    message:"Internal Server Error"
-})
-    }
-}
-
-
-
-module.exports={getallComplaints,getsingleComplaint,updatecomplaintStatus}
+module.exports = {
+  getallComplaints,
+  getsingleComplaint,
+  updatecomplaintStatus,
+  dashboardStats,
+};
--- Backend/routes/admincomplaintRoute.js
+++ Backend/routes/admincomplaintRoute.js
@@ -1,11 +1,13 @@
 const express =require("express");
 const router = express.Router();
-const {getallComplaints,getsingleComplaint, updatecomplaintStatus}= require("../controllers/adminComplaintController");
+const {getallComplaints,getsingleComplaint, updatecomplaintStatus,dashboardStats}= require("../controllers/adminComplaintController");
 const adminAuth =require("../middleware/adminAuth");
 
 router.get("/complaints",adminAuth, getallComplaints);
+router.get("/complaints/dashboard", adminAuth, dashboardStats);
 router.get("/complaints/:complaintId", adminAuth,getsingleComplaint);
 router.put("/complaints/:complaintId",adminAuth,updatecomplaintStatus);
+
 
 module.exports=router;
 
